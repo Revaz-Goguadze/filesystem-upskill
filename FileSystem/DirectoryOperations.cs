8ff239cb1667d654c03448908af4c09cfa08c96d
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-09-05 16:30:12 +0400

    aeae

--- FileSystem/DirectoryOperations.cs
+++ FileSystem/DirectoryOperations.cs
@@ -6,38 +6,164 @@ namespace FileSystem
     {
         public static void DirectoryIsCreatedOrNotValidateDirectory(string dirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (Directory.Exists(dirPath))
+                {
+                    Console.WriteLine($"Directory already exists at: {dirPath}");
+                }
+                else
+                {
+                    // Create the directory.
+                    Directory.CreateDirectory(dirPath);
+                    Console.WriteLine($"Directory created at: {dirPath}");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void DirectoryIsDeletedOrNotValidateDirectory(string dirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (Directory.Exists(dirPath))
+                {
+                    // Delete the directory and its contents.
+                    Directory.Delete(dirPath, true);
+
+                    Console.WriteLine(Directory.Exists(dirPath)
+                        ? "Directory deletion failed."
+                        : $"Directory deleted successfully: {dirPath}");
+                }
+                else
+                {
+                    Console.WriteLine($"Directory does not exist at: {dirPath}");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void DirectoryIsMovedToOtherDirectoryValidateDirectory(string sourceDirPath, string destinationDirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (Directory.Exists(sourceDirPath))
+                {
+                    // Create the destination directory if it doesn't exist.
+                    Directory.CreateDirectory(destinationDirPath);
+
+                    // Move the directory to the destination.
+                    Directory.Move(sourceDirPath, destinationDirPath);
+
+                    Console.WriteLine(Directory.Exists(destinationDirPath)
+                        ? $"Directory moved successfully to: {destinationDirPath}"
+                        : "Directory move operation failed.");
+                }
+                else
+                {
+                    Console.WriteLine($"Source directory does not exist at: {sourceDirPath}");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void SubDirectoryIsCreatedOrNotValidateSubDirectory(string dirPath, string subDirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the subdirectory already exists.
+                if (!Directory.Exists(subDirPath))
+                {
+                    // Create the subdirectory.
+                    Directory.CreateDirectory(subDirPath);
+
+                    Console.WriteLine($"Subdirectory '{subDirPath}' created successfully in '{dirPath}'.");
+                }
+                else
+                {
+                    Console.WriteLine($"Subdirectory '{subDirPath}' already exists in '{dirPath}'.");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during subdirectory creation.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void DeleteSubDirectoryValidateSubDirectoryDeletedOrNot(string subDirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the subdirectory exists before attempting to delete it.
+                if (Directory.Exists(subDirPath))
+                {
+                    // Delete the subdirectory, including its contents.
+                    Directory.Delete(subDirPath, true);
+
+                    // Validate whether the subdirectory was deleted.
+                    Console.WriteLine(!Directory.Exists(subDirPath)
+                        ? $"Subdirectory '{subDirPath}' deleted successfully."
+                        : $"Subdirectory '{subDirPath}' deletion failed.");
+                }
+                else
+                {
+                    Console.WriteLine($"Subdirectory '{subDirPath}' does not exist.");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during subdirectory deletion.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void MoveSubDirectoryValidateSubDirMovedOrNot(string sourcePath, string destinationPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the source subdirectory exists before moving it.
+                if (Directory.Exists(sourcePath))
+                {
+                    // Move the subdirectory to the destination path.
+                    Directory.Move(sourcePath, destinationPath);
+
+                    // Validate whether the subdirectory was moved to the destination.
+                    Console.WriteLine(Directory.Exists(destinationPath)
+                        ? $"Subdirectory moved from '{sourcePath}' to '{destinationPath}'."
+                        : $"Subdirectory move operation failed.");
+                }
+                else
+                {
+                    Console.WriteLine($"Source subdirectory '{sourcePath}' does not exist.");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during subdirectory move.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
--- FileSystem/FileOperations.cs
+++ FileSystem/FileOperations.cs
@@ -9,37 +9,161 @@ namespace FileSystem
         public static void CreatingFileAndReturnFilePath(string filePath)
         {
             // TODO Implement the method.
-            throw new NotImplementedException();
+            using (File.Create(filePath))
+            {
+            }
         }
 
         public static void WriteTextToFileReadAppendedText(string filePath, string msgToWrite)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Append the message to the file. If the file doesn't exist, it will be created.
+                File.AppendAllText(filePath, msgToWrite);
+
+                // Read the contents of the file after appending the text.
+                string appendedText = File.ReadAllText(filePath);
+
+                Console.WriteLine("Appended Text:");
+                Console.WriteLine(appendedText);
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during file operations.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static string ReadingFileContentAndValidateText(string filePath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the file exists.
+                if (File.Exists(filePath))
+                {
+                    // Read the contents of the file.
+                    string fileContent = File.ReadAllText(filePath);
+
+                    // You can add your validation logic here.
+                    // For example, check if the file content contains specific text.
+                    return fileContent;
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist.");
+                    return null!; // Return null to indicate that the file doesn't exist.
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during file reading.
+                Console.WriteLine($"Error: {ex.Message}");
+                return null!; // Return null to indicate an error.
+            }
         }
 
         public static void MoveFileFromOneFolderToNewFolderAndValidateFile(string filePath, string destinationPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the source file exists.
+                if (File.Exists(filePath))
+                {
+                    // Create the destination directory if it doesn't exist.
+                    Directory.CreateDirectory(Path.GetDirectoryName(destinationPath)!);
+
+                    // Move the file to the destination folder.
+                    File.Move(filePath, destinationPath);
+
+                    // Validate whether the file exists in the new location.
+                    if (File.Exists(destinationPath))
+                    {
+                        Console.WriteLine($"File moved successfully to: {destinationPath}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("File move operation failed.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Source file does not exist.");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during file operations.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void CopyFileFromOneFolderToNewFolder(string filePath, string destinationPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the source file exists.
+                if (File.Exists(filePath))
+                {
+                    // Create the destination directory if it doesn't exist.
+                    Directory.CreateDirectory(Path.GetDirectoryName(destinationPath)!);
+
+                    // Copy the file to the destination folder.
+                    File.Copy(filePath, destinationPath, true);
+
+                    Console.WriteLine($"File copied successfully to: {destinationPath}");
+                }
+                else
+                {
+                    Console.WriteLine("Source file does not exist.");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during file operations.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         public static void DeleteFileAndValidateFileExistOrNot(string filePath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                // Check if the file exists before attempting to delete it.
+                if (File.Exists(filePath))
+                {
+                    // Delete the file.
+                    File.Delete(filePath);
+
+                    // Validate whether the file still exists.
+                    if (File.Exists(filePath))
+                    {
+                        Console.WriteLine("File deletion failed.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("File deleted successfully.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File does not exist.");
+                }
+            }
+#pragma warning disable CA1031
+            catch (Exception ex)
+#pragma warning restore CA1031
+            {
+                // Handle any exceptions that may occur during file deletion.
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
